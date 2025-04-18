c04e88340ff8ebdd1ead3748a6bfb29237140c22
eshaza11 <iszafar@protonmail.com>
2025-04-18 10:21:22 -0400

    added Mira

---
+++ src/mira/Task.java
@@ -0,0 +1,20 @@
+package mira;
+
+/*
+Given an array of ints,
+return the number of 9's in the array.
+
+arrayCount9([1, 2, 9]) → 1
+arrayCount9([1, 9, 9]) → 2
+arrayCount9([1, 9, 9, 3, 9]) → 3 */
+
+
+public class Task {
+    public static void main(String[] args) {
+
+    }
+    public int arrayCount9(int[] nums) {
+
+    }
+
+}
