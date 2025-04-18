b9ec5695f80a59183de8abe503698862517c0cf1
eshaza11 <iszafar@protonmail.com>
2025-04-18 10:23:12 -0400

    added Khursheda

---
+++ src/sheda/Task.java
@@ -0,0 +1,27 @@
+package sheda;
+
+/*
+
+Given an array of ints,
+return true if one of the first 4 elements in the array is a 9.
+The array length may be less than 4.
+
+
+arrayFront9([1, 2, 9, 3, 4]) → true
+arrayFront9([1, 2, 3, 4, 9]) → false
+arrayFront9([1, 2, 3, 4, 5]) → false */
+
+
+public class Task {
+    public static void main(String[] args) {
+
+    }
+    public String stringTimes(String str, int n) {
+
+    }
+    public boolean arrayFront9(int[] nums) {
+
+    }
+
+
+}
