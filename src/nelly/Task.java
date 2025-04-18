e24c30eb63bc614371c2dd54e6650184780d47fc
eshaza11 <iszafar@protonmail.com>
2025-04-18 01:01:40 -0400

    task2

--- src/nelly/Task.java
+++ src/nelly/Task.java
@@ -1,7 +1,18 @@
 package nelly;
+/*
+* Given 2 int values,
+*  return true if either of them is in the range 10..20 inclusive.
 
+
+in1020(12, 99) → true
+in1020(21, 12) → true
+in1020(8, 99) → false*/
 public class Task {
     public static void main(String[] args) {
 
     }
+    public boolean in1020(int a, int b) {
+
+    }
+
 }
