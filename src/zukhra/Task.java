e2df4a88c4d6e84490e4868c6573b83b25df24e6
Barlas Zukhra <zukhrarinatovna@gmail.com>
2025-04-18 18:46:16 -0400

    Updated tasks in zukhra branch

--- src/zukhra/Task.java
+++ src/zukhra/Task.java
@@ -15,6 +15,14 @@ public class Task {
     }
     public int arrayCount9(int[] nums) {
 
+       int count = 0;
+       for (int num : nums){
+           if (num == 9){
+               count++;
+           }
+       }
+       return count;
+
     }
 
 }
