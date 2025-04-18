7e6b6f7f6bb6dc81f4594b93154ce680cfa13a85
eshaza11 <iszafar@protonmail.com>
2025-04-18 00:59:08 -0400

    added some tasks

--- src/akmal/Task.java
+++ src/akmal/Task.java
@@ -1,5 +1,16 @@
 package akmal;
+/*
+* We have two monkeys, a and b,
+*  and the parameters aSmile and bSmile indicate if each is smiling.
+*  We are in trouble if they are both smiling
+* or  if neither of them is smiling.
+* Return true if we are in trouble.
 
+ex:
+monkeyTrouble(true, true) → true
+monkeyTrouble(false, false) → true
+monkeyTrouble(true, false) → false
+* */
 public class Task {
     public static void main(String[] args) {
 
--- src/elizat/Task.java
+++ src/elizat/Task.java
@@ -1,5 +1,14 @@
 package elizat;
+/*
+* Given 2 int values,
+*  return true if one is negative and one is positive.
+*  Except if the parameter "negative" is true,
+* then return true only if both are negative.
 
+ex:
+posNeg(1, -1, false) → true
+posNeg(-1, 1, false) → true
+posNeg(-4, -5, true) → true*/
 public class Task {
     public static void main(String[] args) {
 
