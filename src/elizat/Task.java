4e1143504c8ed12bd9276de2af5a2e34f81e5daf
Elizat25 <elizat.tashpolotova@gmail.com>
2025-04-18 20:48:45 -0500

    my first task

--- src/elizat/Task.java
+++ src/elizat/Task.java
@@ -2,6 +2,8 @@ package elizat;
 
 public class Task {
     public static void main(String[] args) {
-
+       String a="hello everyone";
+       String reversed =new StringBuilder(a).reverse().toString();
+        System.out.println("reversed " +reversed);
     }
 }
