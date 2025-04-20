5cf17d416b7cad127b82b86afae41953108f0d4f
Barlas Zukhra <zukhrarinatovna@gmail.com>
2025-04-20 10:52:23 -0400

    Updated Fibonacci logic in Task.java

--- src/zukhra/Task.java
+++ src/zukhra/Task.java
@@ -11,18 +11,40 @@ arrayCount9([1, 9, 9, 3, 9]) → 3 */
 
 public class Task {
     public static void main(String[] args) {
+        Task task = new Task();
+        int[] nums = {1, 2, 9, 9, 3, 9};
+        System.out.println("Number of 9's: " + task.arrayCount9(nums));
 
+        int n = 10;
+        task.getFibonacci(n);
     }
+
+
     public int arrayCount9(int[] nums) {
-
-       int count = 0;
-       for (int num : nums){
-           if (num == 9){
-               count++;
-           }
-       }
-       return count;
-
+        int count = 0;
+        for (int num : nums) {
+            if (num == 9) {
+                count++;
+            }
+        }
+        return count;
     }
 
+    public int getFibonacci(int n) {
+
+        int number = 10;
+        int first = 0;
+        int second = 1;
+
+        System.out.print(first + " " + second + " "); // Print first two numbers
+
+        for (int i = 2; i < n; i++) {
+            int next = first + second;
+            System.out.print(next + " "); // Print the next Fibonacci number
+            first = second;
+            second = next;
+        }
+        return number;
+    }
 }
+
