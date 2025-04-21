85a8a385755a7d5ffd7c43db2db1784b4e51dc4f
KateLakhti <lahtionovak@gmail.com>
2025-04-21 17:13:39 -0500

    Kates first feture

--- src/kate/Task.java
+++ src/kate/Task.java
@@ -9,10 +9,19 @@ hasTeen(20, 19, 10) → true
 hasTeen(20, 10, 13) → true*/
 public class Task {
     public static void main(String[] args) {
+        Task task = new Task();
 
+        System.out.println(task.hasTeen(13, 20, 10));
+        System.out.println(task.hasTeen(20, 19, 10));
+        System.out.println(task.hasTeen(20, 10, 13));
+        System.out.println(task.hasTeen(20, 10, 12));
     }
     public boolean hasTeen(int a, int b, int c) {
+        return isTeen(a) || isTeen(b) || isTeen(c);
+    }
 
+    private boolean isTeen(int n) {
+        return n >= 13 && n <= 19;
     }
 
 }
