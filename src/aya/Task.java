c6522561a2cd574f5599ab31536fc975e161b878
Your Name <aigerimmukhtarkyzy@MacBook-Air-Aigerim.local>
2025-04-21 15:44:39 -0400

    added new task

---
+++ src/aya/Task.java
@@ -0,0 +1,20 @@
+package aya;
+
+import java.util.Scanner;
+
+public class Task {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is an even number.");
+        } else {
+            System.out.println(number + " is an odd number.");
+        }
+
+        scanner.close();
+    }
+}
