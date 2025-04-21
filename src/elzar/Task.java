d70d983c35eb2e240bb50738a726512eca72fc9d
elzar <elzosman4@gmail.com>
2025-04-20 22:51:50 -0500

    Task completed

--- src/elzar/Task.java
+++ src/elzar/Task.java
@@ -1,4 +1,5 @@
 package elzar;
+
 /*
 * Given a string,
 * return true if the string starts with "hi" and false otherwise.
@@ -9,10 +10,19 @@ startHi("hi") → true
 startHi("hello hi") → false*/
 public class Task {
     public static void main(String[] args) {
+        Task task = new Task();
+
+        task.startHi("hi there");
+        task.startHi("HI");
+        task.startHi("hello hi");
+        task.startHi("Hi");
+        task.startHi("hI");
 
     }
+
     public boolean startHi(String str) {
 
+        return str.toLowerCase().startsWith("hi");
     }
 
 }
